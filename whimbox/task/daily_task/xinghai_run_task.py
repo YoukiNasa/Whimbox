a1ddf25604581269cd4896090a6ddc16a06c5009
userkey <5314175+userkey@user.noreply.gitee.com>
2026-01-23 13:28:22 +0800

    优化星海结晶位置的查看

--- whimbox/task/daily_task/xinghai_run_task.py
+++ whimbox/task/daily_task/xinghai_run_task.py
@@ -28,15 +28,50 @@ class XinghaiRunTask(TaskTemplate):
 
     @register_step("查看星光结晶位置")
     def step2(self):
+
+        def move_map(src_posi, dst_posi):
+            itt.move_to(src_posi)
+            itt.left_down()
+            # 瞎几把拖几下地图，防止游戏没反应过来
+            for i in range(5):
+                itt.move_to([10, 10], relative=True)
+                if i % 2 == 0:
+                    itt.left_down()
+            for i in range(5):
+                itt.move_to([-10, -10], relative=True)
+                if i % 2 == 0:
+                    itt.left_down()
+            itt.move_to(dst_posi, anchor=ANCHOR_CENTER)
+            time.sleep(0.2)
+            itt.move_to(dst_posi, anchor=ANCHOR_CENTER)
+            itt.left_up()
+
+        def move_map_to_right_top_corner():
+            nikki_map._move_bigmap((2329, 1534))
+
         ui_control.goto_page(page_bigmap)
         # 阈值0.9，搜索不在当前地图画面的结晶，并点击跳转
         box = find_game_img(GameImgStarCrystal, itt.capture(), threshold=0.90, scale=1)
         if box:
-            itt.move_and_click(area_center(box))
-            itt.wait_until_stable(threshold=0.9995)
-
-        # 阈值0.7，搜索在当前地图画面的结晶
+            if 1500 < box[0] and box[1] < 400:
+                # 如果在右上角，可能会被地图ui覆盖无法点击
+                logger.info("星光结晶在右上角，可能被右上角ui覆盖无法点击，移动地图到右上角")
+                move_map_to_right_top_corner()
+            else:
+                logger.info("星光结晶不在当前地图画面，点击跳转")
+                itt.move_and_click(area_center(box))
+                itt.wait_until_stable(threshold=0.9995)
+        else:
+            # 如果没有，可能已经在当前湖面，可能被右上角地图ui覆盖无法识别
+            box =  find_game_img(GameImgStarCrystal, itt.capture(), threshold=0.70, scale=1)
+            if not box:
+                logger.info("没找到星光结晶，可能被右上角ui覆盖无法识别，移动地图到右上角")
+                move_map_to_right_top_corner()
+            else:
+                logger.info("星光结晶就在当前画面，不需要任何操作")
+        # 移动地图后，再次识别当前地图画面的结晶
         boxes = find_game_img(GameImgStarCrystal, itt.capture(), threshold=0.70, scale=1, count=3)
+
         if boxes and len(boxes) > 0:
             centers = [area_center(box) for box in boxes]
             # 计算三个box的中心坐标（重心）
@@ -49,22 +84,7 @@ class XinghaiRunTask(TaskTemplate):
             return "step_finish"
         
         # 拖到屏幕中心
-        itt.move_to(triangle_center)
-        itt.left_down()
-        # 瞎几把拖几下地图，防止游戏没反应过来
-        for i in range(5):
-            itt.move_to([10, 10], relative=True)
-            if i % 2 == 0:
-                itt.left_down()
-        for i in range(5):
-            itt.move_to([-10, -10], relative=True)
-            if i % 2 == 0:
-                itt.left_down()
-        time.sleep(0.2)
-        itt.move_to((1920/2, 1080/2), anchor=ANCHOR_CENTER)
-        time.sleep(0.2)
-        itt.move_to((1920/2, 1080/2), anchor=ANCHOR_CENTER)
-        itt.left_up()
+        move_map(triangle_center, (1920/2, 1080/2))
 
         self.target_loc = nikki_map.get_bigmap_posi()
         logger.debug(f"星光结晶地图坐标: {self.target_loc}")
@@ -77,6 +97,7 @@ class XinghaiRunTask(TaskTemplate):
             "星海拾光_星光结晶收集_无界枢纽": (1694.0, 2002.0),
             "星海拾光_星光结晶收集_晶簇之谷": (2264.0, 1469.2),
             "星海拾光_星光结晶收集_大舞台": (3282.8, 2437.6),
+            "星海拾光_星光结晶收集_繁星之滨": (2471.6, 1680.8),
         }
         for path_name, loc in auto_path_dict.items():
             if loc[0]-50 <self.target_loc[0] < loc[0]+50 and loc[1]-50 <self.target_loc[1] < loc[1]+50:
